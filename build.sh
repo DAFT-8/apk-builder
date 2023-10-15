6ff27cbe054076a826babaa6eb761f1916ccf974
Yurin Doctrine <bayramyurgidenn@gmail.com>
2023-10-15 21:01:39 +0000

    add gradle

--- build.sh
+++ build.sh
@@ -7,16 +7,24 @@ command -v javac > /dev/null 2>&1 || { echo >&2 "I require openjdk8 but it's not
 command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
 
-[[ -e /tmp/build-tools.zip ]] || wget -O /tmp/build-tools.zip 'https://dl.google.com/android/repository/build-tools_r25-linux.zip' && [[ -d /tmp/build-tools ]] || unzip /tmp/build-tools.zip -d /tmp/build-tools/
-[[ -e /tmp/platforms.zip ]] || wget -O /tmp/platforms.zip 'https://dl.google.com/android/repository/android-16_r05.zip' && [[ -d /tmp/platforms ]] || unzip /tmp/platforms.zip -d /tmp/platforms/
-[[ -e /tmp/platform-tools.zip ]] || wget -O /tmp/platform-tools.zip 'https://dl.google.com/android/repository/platform-tools-latest-linux.zip' && [[ -d /tmp/platform-tools ]] || unzip /tmp/platform-tools.zip -d /tmp/platform-tools/
+#[[ -e /tmp/build-tools.zip ]] || wget -O /tmp/build-tools.zip 'https://dl.google.com/android/repository/build-tools_r25-linux.zip' && [[ -d /tmp/build-tools ]] || unzip /tmp/build-tools.zip -d /tmp/build-tools/
+[[ -e /tmp/commandline-tools.zip ]] || wget -O /tmp/commandline-tools.zip 'https://dl.google.com/android/repository/commandlinetools-linux-9123335_latest.zip' && [[ -d /tmp/commandline-tools ]] || unzip /tmp/commandline-tools.zip -d /tmp/commandline-tools/
+#[[ -e /tmp/platforms.zip ]] || wget -O /tmp/platforms.zip 'https://dl.google.com/android/repository/android-16_r05.zip' && [[ -d /tmp/platforms ]] || unzip /tmp/platforms.zip -d /tmp/platforms/
+#[[ -e /tmp/platform-tools.zip ]] || wget -O /tmp/platform-tools.zip 'https://dl.google.com/android/repository/platform-tools-latest-linux.zip' && [[ -d /tmp/platform-tools ]] || unzip /tmp/platform-tools.zip -d /tmp/platform-tools/
 
-ADB="/tmp/platform-tools/platform-tools/adb"
-AAPT="/tmp/build-tools/android-[0-9]*/aapt"
-DX="/tmp/build-tools/android-[0-9]*/dx"
-ZIPALIGN="/tmp/build-tools/android-[0-9]*/zipalign"
-APKSIGNER="/tmp/build-tools/android-[0-9]*/apksigner"
+ADB="/tmp/platform-tools/adb"
+AAPT="/tmp/build-tools/**/aapt"
+DX="/tmp/build-tools/**/dx"
+ZIPALIGN="/tmp/build-tools/**/zipalign"
+APKSIGNER="/tmp/build-tools/**/apksigner"
 PLATFORM="/tmp/platforms/android-[0-9]*/android.jar"
+SDKMANAGER="/tmp/commandline-tools/cmdline-tools/bin/sdkmanager"
+
+rm -rfd test/
+rm -rfd *.dex
+rm -rfd obj/*
+rm -rfd **/R.java
+rm -rfd mykey.keystore
 
 if [ "$1" == "--install" ]; then
 	echo "Waiting for device..."
@@ -33,10 +41,6 @@ elif [ "$1" == "--test" ]; then
 	[[ -d res/layout ]] || mkdir -p res/layout
 	[[ -d res/values ]] || mkdir res/values
 	[[ -d res/drawable ]] || mkdir res/drawable
-	echo "Cleaning..."
-	rm -rfd obj/*
-	rm -rfd src/com/example/app/R.java
-	rm -rfd mykey.keystore
 	echo "Launching..."
 	echo -e "<?xml version='1.0'?>
 	<manifest xmlns:a='http://schemas.android.com/apk/res/android' package='com.example.helloandroid' a:versionCode='0' a:versionName='0'>
@@ -105,39 +109,27 @@ elif [ "$1" == "--test" ]; then
 	$APKSIGNER sign --ks mykey.keystore bin/hello.unaligned.apk
 	#$ZIPALIGN -f 4 bin/hello.unaligned.apk bin/hello.apk
 
+	echo "Cleaning up the mess..."
+	rm -rfd *.dex
+	rm -rfd obj/*
+	rm -rfd src/com/example/app/R.java
+	rm -rfd mykey.keystore
+
 	$ADB install -r bin/hello.unaligned.apk
 	$ADB shell am start -n com.example.app/.MainActivity
  	exit
 fi
 
-[[ -d libs ]] || mkdir libs
-[[ -d obj ]] || mkdir obj
-[[ -d bin ]] || mkdir bin
+yes | $SDKMANAGER --install "platform-tools" "platforms;android-29" "build-tools;29.0.2" --sdk_root=/tmp/
 
-echo "Cleaning..."
+./gradlew build
+
+echo "Cleaning up the mess..."
+rm -rfd test/
+rm -rfd *.dex
 rm -rfd obj/*
 rm -rfd **/R.java
 rm -rfd mykey.keystore
-
-cd $(dirname $(find . -name "AndroidManifest.xml"))
-
-echo "Generating R.java file..."
-$AAPT package -f -m -J src -M AndroidManifest.xml -S res -I $PLATFORM
-
-echo "Compiling..."
-find . -name "*.java" | xargs javac -d obj -classpath src -bootclasspath $PLATFORM -source 1.7 -target 1.7
-
-echo "Translating to Dalvik byte code..."
-$DX --dex --output=classes.dex obj/
-
-echo "Making APK..."
-$AAPT package -f -m -F bin/debug.unaligned.apk -M AndroidManifest.xml -S res -I $PLATFORM
-$AAPT add bin/debug.unaligned.apk classes.dex
-
-echo "Aligning and signing APK..."
-keytool -genkeypair -validity 365 -keystore mykey.keystore -keyalg RSA -keysize 2048
-$APKSIGNER sign --ks mykey.keystore bin/debug.unaligned.apk
-#$ZIPALIGN -f 4 bin/hello.unaligned.apk bin/hello.apk
 
 echo "All done! Apk at bin. Use --install to install it on your device."
 
